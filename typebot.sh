fb09d7e5112642f000504f7910e9c618ec729596
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-08-14 01:29:09 -0300

    Update typebot.sh

--- typebot.sh
+++ typebot.sh
@@ -3,50 +3,58 @@
 clear
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
-echo -e "\e[32m  ####                        ##               ###                 ###\e[0m"
-echo -e "\e[32m   ##                         ##                ##                  ##\e[0m"
-echo -e "\e[32m   ##     #####     #####    #####    ####      ##      ####        ##    ####    ######\e[0m"
-echo -e "\e[32m   ##     ##  ##   ##         ##         ##     ##         ##    #####   ##  ##    ##  ##\e[0m"
-echo -e "\e[32m   ##     ##  ##    #####     ##      #####     ##      #####   ##  ##   ##  ##    ##\e[0m"
-echo -e "\e[32m   ##     ##  ##        ##    ## ##  ##  ##     ##     ##  ##   ##  ##   ##  ##    ##\e[0m"
-echo -e "\e[32m  ####    ##  ##   ######      ###    #####    ####     #####    ######   ####    ####\e[0m"
 echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m    ###                     ######                              ###                 ##\e[0m"
-echo -e "\e[32m     ##                     # ## #                               ##                 ##\e[0m"
-echo -e "\e[32m     ##    ####               ##     ##  ##   ######    ####     ##       ####     #####\e[0m"
-echo -e "\e[32m  #####   ##  ##              ##     ##  ##    ##  ##  ##  ##    #####   ##  ##     ##\e[0m"
-echo -e "\e[32m ##  ##   ##  ##              ##     ##  ##    ##  ##  ######    ##  ##  ##  ##     ##\e[0m"
-echo -e "\e[32m ##  ##   ##  ##              ##      #####    #####   ##        ##  ##  ##  ##     ## ##\e[0m"
-echo -e "\e[32m  ######   ####              ####        ##    ##       #####   ######    ####       ###\e[0m"
-echo -e "\e[32m                                     #####    ####\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32mBy OrionDesign\e[0m"
+echo -e "\e[32m _                             _              _               \e[0m"
+echo -e "\e[32m| |                _          | |            | |              \e[0m"
+echo -e "\e[32m| | ____    ___  _| |_  _____ | |  _____   __| |  ___    ____ \e[0m"
+echo -e "\e[32m| ||  _ \  /___)(_   _)(____ || | (____ | / _  | / _ \  / ___)\e[0m"
+echo -e "\e[32m| || | | ||___ |  | |_ / ___ || | / ___ |( (_| || |_| || |    \e[0m"
+echo -e "\e[32m|_||_| |_|(___/    \__)\_____| \_)\_____| \____| \___/ |_|    \e[0m"
+echo -e "\e[32m                                                              \e[0m"
+echo -e "\e[32m     _           _______                       _                   \e[0m"
+echo -e "\e[32m    | |         (_______)                     | |              _   \e[0m"
+echo -e "\e[32m  __| |  ___        _     _   _  ____   _____ | |__    ___   _| |_ \e[0m"
+echo -e "\e[32m / _  | / _ \      | |   | | | ||  _ \ | ___ ||  _ \  / _ \ (_   _)\e[0m"
+echo -e "\e[32m( (_| || |_| |     | |   | |_| || |_| || ____|| |_) )| |_| |  | |_ \e[0m"
+echo -e "\e[32m \____| \___/      |_|    \__  ||  __/ |_____)|____/  \___/    \__)\e[0m"
+echo -e "\e[32m                         (____/ |_|                                \e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 
+sleep 3
 #######################################################
 
 read -p "Link do Builder (ex: typebot.seudominio.com): " builder
 echo ""
+read -p "Porta do Builder (padrão: 3001): " portabuilder #novo
+echo ""
 read -p "Link do Viewer (ex: bot.seudominio.com): " viewer
+echo ""
+read -p "Porta do Viewer (padrão: 3002): " portaviewer #novo
 echo ""
 read -p "Link do Storage (ex: storage.seudominio.com): " storage
 echo ""
+read -p "Porta do Storage (padrão: 9000): " portastorage #novo
+echo ""
 read -p "Seu Email (ex: contato@dominio.com): " email
 echo ""
-read -p "Senha do seu Email: " senha
+read -p "Senha do seu Email (se for gmail, precisa ser a senha de aplicativo): " senha
 echo ""
 read -p "SMTP do seu email (ex: smtp.hostinger.com): " smtp
 echo ""
-read -p "Chave secreta de 32 caracteres: " key
+read -p "Porta SMTP (ex: 465): " portasmtp #novo
+echo ""
+read -p "SMTP_SECURE (Se a porta SMTP for 465, digite true, caso contrario,digite false): " SECURE #novo
+echo ""
+read -p "Chave secreta de 32 caracteres: (ex: c56f3775313440c3edce57529a0f02b4) " key
 
 #######################################################
+
+echo ""
+echo ""
+echo ""
 
 echo "Atualizando a VPS + Instalando Docker Compose + Nginx + Certbot"
 
@@ -95,7 +103,7 @@ services:
       - POSTGRES_PASSWORD=typebot
   typebot-builder:
     ports:
-      - 3001:3000
+      - $portabuilder:3000
     image: baptistearno/typebot-builder:latest
     restart: always
     depends_on:
@@ -108,8 +116,11 @@ services:
       - ENCRYPTION_SECRET=$key
 
       - ADMIN_EMAIL=$email
-      
+
+      - SMTP_SECURE=$SECURE
+
       - SMTP_HOST=$smtp
+      - SMTP_PORT=$portasmtp
       - SMTP_USERNAME=$email
       - SMTP_PASSWORD=$senha
       - NEXT_PUBLIC_SMTP_FROM='Suporte Typebot' <$email>
@@ -120,7 +131,7 @@ services:
       - S3_ENDPOINT=$storage
   typebot-viewer:
     ports:
-      - 3002:3000
+      - $portaviewer:3000
     image: baptistearno/typebot-viewer:latest
     restart: always
     environment:
@@ -138,12 +149,12 @@ services:
   minio:
     labels:
       virtual.host: '$storage'
-      virtual.port: '9000'
+      virtual.port: '$portastorage'
       virtual.tls-email: '$email'
     image: minio/minio
     command: server /data
     ports:
-      - '9000:9000'
+      - '$portastorage:$portastorage'
     environment:
       MINIO_ROOT_USER: minio
       MINIO_ROOT_PASSWORD: minio123
@@ -156,7 +167,7 @@ services:
     entrypoint: >
       /bin/sh -c "
       sleep 10;
-      /usr/bin/mc config host add minio http://minio:9000 minio minio123;
+      /usr/bin/mc config host add minio http://minio:$portastorage minio minio123;
       /usr/bin/mc mb minio/typebot;
       /usr/bin/mc anonymous set public minio/typebot/public;
       exit 0;
@@ -197,7 +208,7 @@ server {
 
   location / {
 
-    proxy_pass http://127.0.0.1:3001;
+    proxy_pass http://127.0.0.1:$portabuilder;
 
     proxy_http_version 1.1;
 
@@ -237,7 +248,7 @@ server {
 
   location / {
 
-    proxy_pass http://127.0.0.1:3002;
+    proxy_pass http://127.0.0.1:$portaviewer;
 
     proxy_http_version 1.1;
 
@@ -277,7 +288,7 @@ server {
 
   location / {
 
-    proxy_pass http://127.0.0.1:9000;
+    proxy_pass http://127.0.0.1:$portastorage;
 
     proxy_http_version 1.1;
 
@@ -316,25 +327,21 @@ echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m  ####                        ##               ###                 ###\e[0m"
-echo -e "\e[32m   ##                         ##                ##                  ##\e[0m"
-echo -e "\e[32m   ##     #####     #####    #####    ####      ##      ####        ##    ####\e[0m"
-echo -e "\e[32m   ##     ##  ##   ##         ##         ##     ##         ##    #####   ##  ##\e[0m"
-echo -e "\e[32m   ##     ##  ##    #####     ##      #####     ##      #####   ##  ##   ##  ##\e[0m"
-echo -e "\e[32m   ##     ##  ##        ##    ## ##  ##  ##     ##     ##  ##   ##  ##   ##  ##\e[0m"
-echo -e "\e[32m  ####    ##  ##   ######      ###    #####    ####     #####    ######   ####\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32mAcesse o Builder através do link: $builder\e[0m"
-echo -e "\e[32m\e[0m"
-echo -e "\e[32mInscreva-se no meu Canal: youtube.com/oriondesign_oficial\e[0m"
+echo -e "\e[32m _                             _              _        \e[0m"
+echo -e "\e[32m| |                _          | |            | |       \e[0m"
+echo -e "\e[32m| | ____    ___  _| |_  _____ | |  _____   __| |  ___  \e[0m"
+echo -e "\e[32m| ||  _ \  /___)(_   _)(____ || | (____ | / _  | / _ \ \e[0m"
+echo -e "\e[32m| || | | ||___ |  | |_ / ___ || | / ___ |( (_| || |_| |\e[0m"
+echo -e "\e[32m|_||_| |_|(___/    \__)\_____| \_)\_____| \____| \___/ \e[0m"
+echo -e "\e[32m                                                       \e[0m"              
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
+echo -e "\e[32mAcesse o Builder através do link: https://$builder\e[0m"
 echo -e "\e[32m\e[0m"
+echo -e "\e[32mInscreva-se no meu Canal: https://youtube.com/oriondesign_oficial\e[0m"
 echo -e "\e[32m\e[0m"
+echo -e "\e[32mSugestões ou duvidas: https://wa.me/+5511973052593\e[0m"
 echo -e "\e[32m\e[0m"
 echo -e "\e[32m\e[0m"
