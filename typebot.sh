8ca26539e3c00986e1d15d46363ab1ac6fe31b98
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-09-04 12:42:27 -0300

    Update typebot.sh

--- typebot.sh
+++ typebot.sh
@@ -108,7 +108,7 @@ services:
   typebot-builder:
     ports:
       - $portabuilder:3000
-    image: baptistearno/typebot-builder:main
+    image: baptistearno/typebot-builder:latest
     restart: always
     depends_on:
       - typebot-db
@@ -136,7 +136,7 @@ services:
   typebot-viewer:
     ports:
       - $portaviewer:3000
-    image: baptistearno/typebot-viewer:main
+    image: baptistearno/typebot-viewer:latest
     restart: always
     environment:
       - DATABASE_URL=postgresql://postgres:typebot@typebot-db:5432/typebot
